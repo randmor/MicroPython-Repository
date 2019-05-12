b542bda57b6b34ac26ebac9d8ee9bbdbb7d8351b
William Moore <randmor@yahoo.com>
2019-05-12 11:03:41 -0700

    Add files via upload

--- TTP229_KeyPad_Demo.py
+++ TTP229_KeyPad_Demo.py
@@ -1,24 +1,34 @@
 #
 # Test/Demo program for TTP229 based 16-Key KeyPad
 #
-# Works okay for keys 1-8 if MAX = 8. When MAX = 16,
-# the keys marked 1 thru 8 return values 9 thru 16 and
-# keys marked 9 thru 16 return nothing. Is likely a
-# keypad configuration problem, need to research more.
+# Seems not to need the P1-3 jumper as indicated in the
+# Hobby Components HCMODU0079_Arduino_Diagram.png image file
+# (see second URL below). Originally, I had a problem where
+# only the first 8 keys of the keypad were responsive, but
+# erroneously gave me key numbers in the range of 9 to 16.
+# I was able to fix the code to correct the number mismatch
+# by counting only to 8 (or 9 as required by range() function).
+# But, my goal was to get all 16 keys working, so I then
+# soldered on the jumper headers to see if I could configure
+# the keypad board to be responsive to all 16 keys. In doing
+# that (but without using any actual jumpers) the keypad
+# mysteriously started to work right -- all 16 keys were
+# responsive. Fiddling with the hardware some more, the old
+# problem came back. Since the software wasn't changed, I
+# posted it up here for safe keeping on GitHub, and if anyone
+# else wants to give it a try, go ahead... In the meantime,
+# I think I will order a couple more of these boards to see if
+# I can get them to work reliably on the Microbit to overcome
+# some of the input limitations of the device.
 #
-# If I jumper P1-3, keypad forever returns "11". So, the
-# Arduino-to-Keypad wiring diagram image showing how the
-# keypad board should be jumpered seems wrong. Here's that URL:
+# Here's the URL for the C/C++ demo I got from Hobby Components:
 '''
 http://forum.hobbycomponents.com/viewtopic.php?f=73&t=1781&hilit=hcmodu0079
 http://hobbycomponents.com/images/forum/HCMODU0079_Arduino_Diagram.png
 
 '''
-# This is my second attempt to rewrite C/C++ script
-# into MicroPython so I can use this 16-key keypad on
-# the BBC Microbit/MicroPython platform.
 #
-# Filename: keyPadTest2.py"
+# Filename: "TTP229_KeyPad_Demo.py"
 #
 
 from microbit import *
